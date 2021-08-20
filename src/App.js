c0809641e9cd0538ff9bc53b644e98975c56c516
Nilava Pal <83910597+palnilava@users.noreply.github.com>
2021-08-20 14:02:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 function App() {
   return (
     <div className="App">
-      <h1>IIC Internship Portal FrontEnd</h1>
+    <Router>
+      <Switch>
+      <Route path="/Signup" component={Signup}/>
+      <Route path="/Login" component={Login}/>
+      </Switch>
+    </Router>
     </div>
   );
 }
